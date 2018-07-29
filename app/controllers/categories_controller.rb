1e2f34303c1da6582bce2684e36c161a5b982072
PavelProtchenko <protchenkopa@gmail.com>
2018-07-29 13:44:56 +0300

    Fix some bugs with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,6 +12,7 @@ class CategoriesController < ApplicationController
 
   def new
     @category = Category.new
+    @categories = Category.all.order(:name)
   end
 
   def create
@@ -19,18 +20,21 @@ class CategoriesController < ApplicationController
     if @category.save
       redirect_to categories_path, success: 'Category was successfully created'
     else
+      @categories = Category.all.order(:name)
       flash.now[:danger] = 'Category was not created'
       render :new
     end
   end
 
   def edit
+    @categories = Category.where("id != #{@category.id}").order(:name)
   end
 
   def update
     if @category.update_attributes(category_params)
       redirect_to categories_path, success: 'Category was successfully updated'
     else
+      @categories = Category.where("id != #{@category.id}").order(:name)
       flash.now[:danger] = 'Category was not updated'
       render :edit
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   belongs_to :category
 
